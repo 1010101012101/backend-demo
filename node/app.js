f0c4d4692bf73cc6dc6203cdf352bd28689849e3
Ben Thompson <misterpink14@gmail.com>
2016-06-29 20:48:48 -0600

    Added simple node app

---
+++ node/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', function(request, response) {
+    response.send("Hello world")
+})
+
+
+app.listen(7000, function() {
+    console.log("App is running on port 7000")
+})
